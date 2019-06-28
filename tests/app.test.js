b224f28043564b3441da306bbec0ff8c9d9a410f
Dirt Deodara <thecrowleaps@gmail.com>
2019-06-28 14:46:43 -0700

    wrote test and added to the fuction to manage POST

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,13 +1,23 @@
 
 const request = require('supertest');
-const app = require('../lib/app');
+const { app } = require('../lib/app');
 
 describe('', () => {
-  it('able to parse request in server', () => {
+  it('able to parse request and send response for GET /', () => {
     return request(app)
       .get('/')
       .then(res => {
-        
+        expect(res.status).toEqual(200);
+        expect(res.text).toEqual(expect.stringContaining('hi'));
+      });
+  });
+
+  it('able to parse request and send response for POST /', () => {
+    return request(app)
+      .post('/')
+      .then(res => {
+        expect(res.status).toEqual(400);
+        expect(res.text).toEqual(expect.stringContaining('sorry'));
       });
   });
 });
